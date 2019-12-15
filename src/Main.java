5c2aed0f3b3b897b565cf9f33917e1ca56fad946
MaJoRShS <lukas.mr@hotmail.com>
2019-12-15 18:32:42 -0300

    Inclusão das aulas 23 a 26
    Ainda estou analizandizando como fazer para organizar os arqivos no repo da melhor forma, para mater os exemplos visto em aula e os exercicios.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,126 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
+	private static Scanner sc;
+
 	public static void main(String[] args) {
 		
-		double y = 10.35784;
-		String nome = "Maria";
-		int idade = 31;
-		double renda = 4000.0;
-		System.out.printf("%.4f%n",y);
 		Locale.setDefault(Locale.US);
-		System.out.printf("%.4f%n",y);
-		System.out.println("Texto qualquer " + y + " Final do texto qualquer");
-		System.out.printf("Resultado = %.2f  Metros%n", y);
-		System.out.printf("%s tem %d anos e ganha R$ : %.2f reais %n", nome, idade, renda);
+		sc = new Scanner(System.in);
+//		double y = 10.35784;
+//		String nome = "Maria";
+//		int idade = 31;
+//		double renda = 4000.0;
+//		System.out.printf("%.4f%n",y);
+//		Locale.setDefault(Locale.US);
+//		System.out.printf("%.4f%n",y);
+//		System.out.println("Texto qualquer " + y + " Final do texto qualquer");
+//		System.out.printf("Resultado = %.2f  Metros%n", y);
+//		System.out.printf("%s tem %d anos e ganha R$ : %.2f reais %n", nome, idade, renda);
+	
+	
+//		int x, y;
+//		x = 5;
+//		y = 2 * x;
+//		System.out.println(x);
+//		System.out.println(y);
+//		
+//		int x;
+//		double y;
+//		x = 5;
+//		y = 2 * x;
+//		System.out.println(x);
+//		System.out.println(y);
+//		
+//		
+//		
+//		double b, B, h, area;
+//		b = 6.0;
+//		B = 8.0;
+//		h = 5.0;
+//		area = (b + B) / 2.0 * h;
+//		System.out.println(area);
+//		
+//		
+//		double b, B, h, area;
+//		b = 6.0;
+//		B = 8.0;
+//		h = 5.0;
+//		area = (b + B) / 2.0 * h;
+//		System.out.println(area);
+//		
+//		
+//		
+//		float b, B, h, area;
+//		b = 6f;
+//		B = 8f;
+//		h = 5f; 
+//		area = (b + B) / 2f * h;
+//		System.out.println(area);
+//		
+//		
+//		int a, b;
+//		double resultado;
+//		a = 5;
+//		b = 2;
+//		resultado = (double) a / b;
+//		System.out.println(resultado);
+//		
+//		
+//		
+//		double a;
+//		int b;
+//		a = 5.0;
+//		b = (int) a;
+//		System.out.println(b);
+		
+//		Scanner sc = new Scanner(System.in);
+//		String x;
+//		int y;
+//		double z;
+//		x = sc.next();
+//		y = sc.nextInt();
+//		z = sc.nextDouble();
+//		System.out.println("DADOS DIGITADOS	:");
+//		System.out.println(x);
+//		System.out.println(y);
+//		System.out.println(z);
+//		sc.close();
+		
+		
+		
+//		System.out.println("Ler texto até a quebra de linha");
+//		Scanner sc = new Scanner(System.in);
+//		String s1, s2, s3;
+//		s1 = sc.nextLine();
+//		s2 = sc.nextLine();
+//		s3 = sc.nextLine();
+//		System.out.println("DADOS DIGITADOS:");
+//		System.out.println(s1);
+//		System.out.println(s2);
+//		System.out.println(s3);
+//		sc.close();
+
+		
+		
+		
+
+	    int x;
+	    String s1, s2, s3;
+	    x = sc.nextInt();
+	    sc.nextLine();
+	    s1 = sc.nextLine();
+	    s2 = sc.nextLine();
+	    s3 = sc.nextLine();
+	    System.out.println("DADOS DIGITADOS:");
+	    System.out.println(x);
+	    System.out.println(s1);
+	    System.out.println(s2);
+	    System.out.println(s3);
+
+		sc.close();
 	}
 
 }
