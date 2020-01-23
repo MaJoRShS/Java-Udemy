e53792e31c73aa7e1d299bf522b3bbe5ffd5311c
Mythings <lucas.rodrigues@original.com.br>
2020-01-23 17:44:46 -0300

    exercício de Fixação
    Aqui eu completei o exercicio de fixação mais mesmo assim estava meio errado, porque na versão dele ele já criou um array com 10 posições e ai depois ele itera o objeto passando esse objeto nas posições do array

--- Seção 10/execicioFixacaoSecao10/src/application/Program.java
+++ Seção 10/execicioFixacaoSecao10/src/application/Program.java
@@ -4,41 +4,45 @@ import java.util.Scanner;
 
 import entities.Person;
 
+/*
+ * Aqui está diferente o meu, mais eu ainda assim cheguei bem perto, e eu só esqueci de fazer o array com 10 quartos.
+ * 
+ * Esse é o que eu fiz, mais não está correto porque não tem as 10 posições, vou criar um igual o dele para consultar depois
+ * 
+ */
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-			
+
 		System.out.print("How many rooms will be rented? ");
 		int qtd = sc.nextInt();
-		
+
 		Person[] vect = new Person[qtd];
-		
-		for(int i =0; i < vect.length; i++) {
-			
-		System.out.println("");
-		System.out.println("Rent #");
-		System.out.print("Name: ");
-		String name = sc.next();
-		
-		System.out.print("Email: ");
-		String mail = sc.next();
-		
-		System.out.print("Room: ");
-		int room = sc.nextInt();
-		
-		vect[i] = new Person(name, mail, room);
-		
+
+		for (int i = 0; i < vect.length; i++) {
+
+			System.out.println("");
+			System.out.println("Rent #:" + (i + 1));
+			System.out.print("Name: ");
+			String name = sc.next();
+
+			System.out.print("Email: ");
+			String mail = sc.next();
+
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+
+			vect[i] = new Person(name, mail, room);
+
 		}
-		
+
 		System.out.println("Busy rooms:");
-		
-		for(int i = 0; i < vect.length; i++){
-		System.out.println(vect[i].toString());
+
+		for (int i = 0; i < vect.length; i++) {
+			System.out.println(vect[i].toString());
 		}
-		
-		
+
 		sc.close();
 	}
 
