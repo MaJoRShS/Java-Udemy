f8589d6d56259d337f1e347a32ab25af2741c463
MaJoRShS <lukas.mr@hotmail.com>
2020-05-25 22:14:06 -0300

    aula 229. Preparação do primeiro projeto no Eclipse - PARTE 2
    Finalizamos  classe de coneção com sucesso no meu fedora

--- Seção 21/jdbc1/src/db/DB.java
+++ Seção 21/jdbc1/src/db/DB.java
@@ -1,5 +1,58 @@
 package db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DB {
 
+	private static Connection conn = null;
+
+	/*
+	 * Aqui está o método que vai abrir a coneção com o banco, ele primeiro verifica
+	 * se a coneção não existe e ai ele começa a instanciala, ele atribui as
+	 * propriedades direto em variaveis do tipo properties mesmo e ai usa esse tal
+	 * de DRIVEMANAGER para abrir a coneção;
+	 */
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			}catch (SQLException e){
+				throw new DbException(e.getMessage());
+			}
+			
+		}
+	}
+
+	/*
+	 * Carrega as propriedades do arquivo db.properties e trás para ca para poder
+	 * abrir a coneção
+	 */
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
 }
